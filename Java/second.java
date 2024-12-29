5df0f7e03d8fd10448d795b1aa4d8b8dc0e001c0
MD TABISH FARHAN <150028841+tabishfarhan7@users.noreply.github.com>
2024-12-29 07:59:07 +0530

    Create second.java

---
+++ Java/second.java
@@ -0,0 +1,9 @@
+public class second{
+    public static void main(String[] args){
+        String name = "Tabish";
+        int age = 24;
+        double price = 22.22;
+        double sum = age + price;
+        System.out.println(sum);
+    }
+}
